e75a4e3831ecfaa03b8547b9f97810ec515edcfd
Alexander Dollard <alexdollard141@gmail.com>
2026-04-04 21:56:49 -0500

    initial commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '141 Web Studios';
+export const SITE_DESCRIPTION = 'Lean, Beautiful Websites For Small Businesses';
